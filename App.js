3239a8f7fe69f56b855a66db814931c3035e2020
EFE SAMED KIRLI <efesamed@outlook.com.tr>
2024-04-22 23:19:58 +0300

    router.js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 // import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+// import { SafeAreaFrameContext } from 'react-native-safe-area-context';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { Home, Profile } from './src/Screens';
+import Routes from './src/Navigation/Routes';
 
 
-const Stack = createNativeStackNavigator(); // fonksiyonu, yığın tabanlı bir navigasyon sistemi oluşturur.
+
+// const Stack = createNativeStackNavigator(); // fonksiyonu, yığın tabanlı bir navigasyon sistemi oluşturur.
 
 // const  HomeScreen = () => {
 //   return (
@@ -28,30 +33,32 @@ const Stack = createNativeStackNavigator(); // fonksiyonu, yığın tabanlı bir
 
 const App = () => {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' >  
-      <Stack.Screen name="Profile" component={ProfileScreen} />  
-        <Stack.Screen name="Home" component={HomeScreen} />
+    <View style={{flex:1}}>
+      <Routes/>
+      <Text>Open up App.js to start working on your app!</Text>
+      
+     
+    </View> 
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName='Home' >  
+    //   <Stack.Screen name="Profile" component={Profile} />  
+    //     <Stack.Screen name="Home" component={Home} />
        
-      </Stack.Navigator>
-    </NavigationContainer>
+    //   </Stack.Navigator>
+    // </NavigationContainer>
 
 
-
+      
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 export default App;
--- src/Navigation/Routes.js
+++ src/Navigation/Routes.js
@@ -2,20 +2,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+// import Home from '../Screens/Home/Home';
+// import Profile from '../Screens/Profile/Profile';
+import { Home,Profile } from '../Screens';
 
 const Stack = createNativeStackNavigator(); // fonksiyonu, yığın tabanlı bir navigasyon sistemi oluşturur.
 
 const Routes = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home' >
-                <Stack.Screen name="Profile" component={ProfileScreen} />
-                <Stack.Screen name="Home" component={HomeScreen} />
-
+            <Stack.Navigator initialRouteName="Home" >
+                <Stack.Screen name="Profile" component={Profile } />
+                <Stack.Screen name="Home" component={Home } />
             </Stack.Navigator>
         </NavigationContainer>
     )
 }
 
-export default Routes
+export default Routes;
